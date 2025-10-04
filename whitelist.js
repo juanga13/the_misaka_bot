e660632755acefcf1a8baea1cebc80b76137d5e7
EC2 Default User <ec2-user@ip-172-31-28-181.sa-east-1.compute.internal>
2025-10-04 04:20:31 +0000

    service setup

--- whitelist.js
+++ whitelist.js
@@ -1,8 +1,8 @@
 const WHITELIST_IDS = [
     {name: "test", id: "-497732391"},
-    {name: "weebs1", id: "-449065093"},
-    {name: "weebs2", id: "-556674928"},
-    {name: "weebs1_2", id: "-1001635179869"},
+//    {name: "weebs1", id: "-449065093"},
+//    {name: "weebs2", id: "-556674928"},
+//    {name: "weebs1_2", id: "-1001635179869"},
 ]
 module.exports = {
     WHITELIST_IDS,
